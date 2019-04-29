094f358c22479fce35d11196ca4819c42dd7b970
saiananya <46471756+saiananya@users.noreply.github.com>
2019-04-29 17:47:33 +0530

    Add files via upload

---
+++ thingspeak.m
@@ -0,0 +1,8 @@
+for i=0:2:20
+thingSpeakWrite(748031,2.8,'WriteKey','ITY8SFBXF9YML1RP')
+pause(20)
+end
+
+%t=0:0.01:2
+%thingSpeakRead(748031,'Fields',1,'OutputFormat','TimeTable')
+%thingSpeakWrite(748031,sin(pi*t),'WriteKey','ITY8SFBXF9YML1RP')
